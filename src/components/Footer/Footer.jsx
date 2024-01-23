4a24345d87735e5a458d7951633246cacf507a76
0Taner <tnr404@gmail.com>
2024-01-23 18:34:47 +0300

    git ignore düzenlendi

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,3 @@
-// import React from 'react'
-
-// function Footer() {
-//     return (
-//         <footer className="footer">
-//           <div className="footer-content">
-//             <p>Taner Can AKTÜRK</p>
-//             <div className="social-links">
-//               <p>
-//                  <a href="https://www.linkedin.com/in/taner-can-akt%C3%BCrk/" target="_blank" rel="noopener noreferrer">
-//                 LinkedIn:</a>
-//               </p>
-//               <p> <a href="https://github.com/0Taner" target="_blank" rel="noopener noreferrer">
-//                 GitHub
-//               </a>
-//               </p>
-//             </div>
-//           </div>
-//         </footer>
-//       );
-//     }
-    
-//     export default Footer;
-
 import React from 'react';
 import "./Footer.css";
 import github from '/icons/github.png';
